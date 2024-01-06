5528ff66deac43ce9d85719b7733dab5415f315f
royal0959 <70254342+royal0959@users.noreply.github.com>
2024-01-06 21:54:19 +0700

    failed attempt at making a different way to do robot model with human animation

--- src/scripts/hazardous_horizons_custom_weapons.lua
+++ src/scripts/hazardous_horizons_custom_weapons.lua
@@ -1,3 +1,27 @@
+local CLASS_ROBOT_MODELS = {
+	[1] = "models/bots/scout/bot_scout.mdl",
+	[2] = "models/bots/sniper/bot_sniper.mdl",
+	[3] = "models/bots/soldier/bot_soldier.mdl",
+	[4] = "models/bots/demo/bot_demo.mdl",
+	[5] = "models/bots/medic/bot_medic.mdl",
+	[6] = "models/bots/heavy/bot_heavy.mdl",
+	[7] = "models/bots/pyro/bot_pyro.mdl",
+	[8] = "models/bots/spy/bot_spy.mdl",
+	[9] = "models/bots/engineer/bot_engineer.mdl",
+}
+
+function ApplyPlayerRobotModel(_, activator)
+	local class = activator.m_iClass
+
+	local robotModel = CLASS_ROBOT_MODELS[class]
+
+	if not robotModel then
+		return
+	end
+
+	activator:SetCustomModelWithClassAnimations(robotModel)
+end
+
 function DronePlaced(_, originalBuilding)
 	print(originalBuilding)
 	if originalBuilding.m_bDisposableBuilding ~= 0 then
